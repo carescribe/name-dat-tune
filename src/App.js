c2c0d15d2395effec23348e07b4b0673f1d51c8f
Tom Cartwright <tecartwright@gmail.com>
2022-04-07 09:46:38 +0100

    Update with heroku version of server

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = io(`http://${window.location.hostname}:3000`);
+    const newSocket = io(`https://name-dat-tune-server.herokuapp.com`);
     setSocket(newSocket);
     return () => newSocket.close();
   }, [setSocket]);
